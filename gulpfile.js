b43f538f511f5f1aa9e7ce19ee19a15e906e3219
Ark1700 <Ark1700@yandex.ru>
2019-12-25 20:24:00 +0300

    Правки перед защитой 2

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,13 @@ var sourcemap = require("gulp-sourcemaps");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var del = require("del");
 var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -16,14 +22,25 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -31,7 +48,47 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", gulp.series("html"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "!source/img/sprite/*",
+    "!source/img/sprite.svg",
+    "source/js/**",
+    "source/*.ico",
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("images", function () {
+  return gulp.src([
+    "source/img/**/*.{png,jpg,svg}",
+    "!source/img/sprite.svg",
+    "!source/img/sprite/*"
+  ])
+  .pipe(imagemin([
+    imagemin.optipng({optimizationlevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "images",
+  "css",
+  "html"
+));
+
+gulp.task("start", gulp.series("build", "server"));
